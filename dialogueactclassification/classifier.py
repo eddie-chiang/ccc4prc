dd0f006ce6298fa37841f5940958c2edd69e8a7d
Eddie Chiang <echiang@fraedom.com>
2019-04-30 15:28:39 +1200

    Change Classifier to a class, so we can set a global logger later.

--- dialogueactclassification/classifier.py
+++ dialogueactclassification/classifier.py
@@ -1,8 +1,12 @@
 import nltk
 
-# Define a simple feature extractor that checks what words the post contains.
-def dialogue_act_features(post):
-    features = {}
-    for word in nltk.word_tokenize(post):
-        features['contains({})'.format(word.lower())] = True
-    return features
+class Classifier:
+    def __init__(self, logger):
+        self.logger = logger
+
+    # Define a simple feature extractor that checks what words the post contains.
+    def dialogue_act_features(self, post):
+        features = {}
+        for word in nltk.word_tokenize(post):
+            features['contains({})'.format(word.lower())] = True
+        return features
