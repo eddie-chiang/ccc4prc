80b3cef40a80f61fe061bb86bce963ac954bd83c
Eddie Chiang <echiang@fraedom.com>
2019-04-30 12:41:22 +1200

    Moved Dialogue Act Features classification to a sub module.

---
+++ dialogueactclassification/classifier.py
@@ -0,0 +1,8 @@
+import nltk
+
+# Define a simple feature extractor that checks what words the post contains.
+def dialogue_act_features(post):
+    features = {}
+    for word in nltk.word_tokenize(post):
+        features['contains({})'.format(word.lower())] = True
+    return features
