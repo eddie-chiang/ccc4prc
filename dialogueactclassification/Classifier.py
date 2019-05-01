7b3767b06c98686790fce9121e5939842ddbd6c0
Eddie Chiang <echiang@fraedom.com>
2019-05-01 16:14:38 +1200

    Move the decision to train/retrain and save/load dialogue act classifier into the submodule.

--- dialogueactclassification/Classifier.py
+++ dialogueactclassification/Classifier.py
@@ -1,19 +1,59 @@
 import collections
 import nltk
+import pickle
 from nltk.metrics.scores import precision, recall
 
 class Classifier:
-    def __init__(self, logger):
+    """A classifier for dialogue act classification.
+
+    Args:
+        logger (Logger): A logger.
+        trained_classifier_file (Path): A Path object that points to the trained classifier .pickle file.
+        train_classifier (bool): If the trained classifier .pickle file already exists, whether to retrain the classifier.
+            If the .pickle file does not exist, then a new dialogue act classifier will be trained, and save to trained_classifier_file.
+    """
+    def __init__(self, logger, trained_classifer_file, train_classifier: bool):
         self.logger = logger
 
-    # Define a simple feature extractor that checks what words the post contains.
-    def dialogue_act_features(self, post):
+        if trained_classifer_file.is_file() and train_classifier == False:
+            with open(trained_classifer_file, mode='rb') as f:
+                self.dialogue_act_classifier = pickle.load(f)
+                logger.info('Loaded trained dialogue act classifier.')
+            
+        else:
+            logger.info('Training dialogue act classifier.')
+            self.dialogue_act_classifier = self.__train()
+            
+            with open(trained_classifer_file, mode='wb') as f:
+                pickle.dump(self.dialogue_act_classifier, f)
+                logger.info('Saved trained dialogue act classifier.')   
+
+    def dialogue_act_features(self, post: str):
+        """Return a list of words extracted from the given post, as features for the post.
+
+        Args:
+            post (str): A sentence, a passage.
+        
+        Returns:
+            A list of tuples ``(contains({word}), True)``.
+        """
         features = {}
         for word in nltk.word_tokenize(post):
             features['contains({})'.format(word.lower())] = True
-        return features
-    
-    def train(self):
+        return features        
+
+    def classify(self, featureset):
+        """Classify the given featureset.
+
+        Args:
+            featureset: A list of features.
+
+        Returns: 
+            The dialogue act classification.
+        """
+        return self.dialogue_act_classifier.classify(featureset)
+
+    def __train(self):
         # Extract the labeled basic messaging data.
         #posts = nltk.corpus.nps_chat.xml_posts()[:10000]
         posts = nltk.corpus.nps_chat.xml_posts()
