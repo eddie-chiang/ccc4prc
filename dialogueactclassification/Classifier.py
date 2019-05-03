4982e6bd09a727be15dbfe4d8e36341ef56325de
Eddie Chiang <echiang@fraedom.com>
2019-05-03 16:24:32 +1200

    Always show the accuracy, precision and recall rates regardless of training a new classifier or loading from .pickle.

--- dialogueactclassification/Classifier.py
+++ dialogueactclassification/Classifier.py
@@ -15,20 +15,22 @@ class Classifier:
     """
     def __init__(self, logger, trained_classifer_file, train_classifier: bool, test_set_percentage: int):
         self.logger = logger
-        self.test_set_percentage = test_set_percentage
 
+        test_set = None
         if trained_classifer_file.is_file() and train_classifier == False:
             with open(trained_classifer_file, mode='rb') as f:
                 self.dialogue_act_classifier = pickle.load(f)
                 logger.info('Loaded trained dialogue act classifier.')
-            
+            _, _, test_set = self.__get_featuresets(test_set_percentage)            
         else:
             logger.info('Training dialogue act classifier.')
-            self.dialogue_act_classifier = self.__train()
+            self.dialogue_act_classifier, test_set = self.__train(test_set_percentage)
             
             with open(trained_classifer_file, mode='wb') as f:
                 pickle.dump(self.dialogue_act_classifier, f)
                 logger.info('Saved trained dialogue act classifier.')   
+
+        self.__log_accuracy_precision_recall(self.dialogue_act_classifier, test_set)
 
     def classify(self, dialgoue: str):
         """Classify the given featureset.
@@ -43,36 +45,21 @@ class Classifier:
         return self.dialogue_act_classifier.classify(unlabeled_data_features)
 
     def __dialogue_act_features(self, dialogue: str):
-        """Return a list of words extracted from the given post, as features for the post.
-
-        Args:
-            dialogue (str): A sentence, a passage.
-        
-        Returns:
-            A list of tuples ``(contains({word}), True)``.
-        """
         features = {}
         for word in nltk.word_tokenize(dialogue):
             features['contains({})'.format(word.lower())] = True
         return features        
 
-    def __train(self):
-        # Extract the labeled basic messaging data.
-        posts = nltk.corpus.nps_chat.xml_posts()
-        self.logger.info(f'Loaded {len(posts)} posts from nps_chat corpus.')
-
-        # Construct the training and test data by applying the feature extractor to each post, and create a new classifier.
-        featuresets = [(self.__dialogue_act_features(post.text), post.get('class'))
-                        for post in posts]
-        test_set_size = int(len(featuresets) * self.test_set_percentage / 100)
-        train_set, test_set = featuresets[test_set_size:], featuresets[:test_set_size]
+    def __train(self, test_set_percentage: int):
+        featuresets, train_set, test_set = self.__get_featuresets(test_set_percentage)
         self.logger.info('Size of feature set: %d, train on %d instances, test on %d instances.' % (len(featuresets), len(train_set), len(test_set)))
 
         # Train the dialogue act classifier.
-        dialogue_act_classifier = nltk.NaiveBayesClassifier.train(train_set)
+        return nltk.NaiveBayesClassifier.train(train_set), test_set
+
+    def __log_accuracy_precision_recall(self, dialogue_act_classifier, test_set: []):
         self.logger.info('Accuracy: {}%.'.format(round(nltk.classify.accuracy(dialogue_act_classifier, test_set) * 100, 4)))
 
-        # Getting the Precision (% of prediction that are correct), and Recall (% of that identifies the class accurately).
         refsets = collections.defaultdict(set)
         testsets = collections.defaultdict(set)
 
@@ -88,7 +75,17 @@ class Classifier:
                 precision_rate = 0
             if not isinstance(recall_rate, float):
                 recall_rate = 0
-            self.logger.info(f'Precision ({class_label}): {round(precision_rate * 100, 4)}%.')
-            self.logger.info(f'Recall ({class_label}): {round(recall_rate * 100, 4)}%.')
+            self.logger.info(f'Precision ({class_label}): {round(precision_rate * 100, 4)}%.') # % of prediction that are correct
+            self.logger.info(f'Recall ({class_label}): {round(recall_rate * 100, 4)}%.') # % of that identifies the class accurately
 
-        return dialogue_act_classifier
+    def __get_featuresets(self, test_set_percentage: int):
+        # Extract the labeled basic messaging data.
+        posts = nltk.corpus.nps_chat.xml_posts()
+
+        # Construct the train and test data by applying the feature extractor to each post, and create a new classifier.
+        featuresets = [(self.__dialogue_act_features(post.text), post.get('class'))
+                        for post in posts]
+        test_set_size = int(len(featuresets) * test_set_percentage / 100)
+        train_set, test_set = featuresets[test_set_size:], featuresets[:test_set_size]
+
+        return featuresets, train_set, test_set
