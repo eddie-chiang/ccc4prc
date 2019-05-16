5277c4938470c495733bd6ed7c3932bda2e563f9
Eddie Chiang <echiang@fraedom.com>
2019-05-16 16:20:40 +1200

    BigQuery to .csv.

--- bigquery/CommentsResourceAccess.py
+++ bigquery/CommentsResourceAccess.py
@@ -1,13 +1,19 @@
+import csv
 import logging
+from csv import DictWriter
 from google.cloud import bigquery
 
 class CommentsResourceAccess:
     """A resource access for Google BigQuery to get pull request comments from GH Torrent.
-
-    https://googleapis.github.io/google-cloud-python/latest/bigquery/index.html
+    
+    Args:
+        query_file (Path): A Path object that points to BigQuery .sql file.    
+        output_csv_file (Path): A Path object that points to output .csv file.    
     """
-    def __init__(self):
+    def __init__(self, query_file, output_csv_file):
         self.logger = logging.getLogger(self.__class__.__name__)
+        self.query_file = query_file
+        self.output_csv_file = output_csv_file
 
     def export(self):
         """Execute the given BigQuery query, and export the result to a .csv file.
@@ -15,13 +21,19 @@ class CommentsResourceAccess:
         Args:
             output_csv_file (Path): A Path object that points to the .csv file to export data to.
         """
-        client = bigquery.Client()
+        with open(self.query_file, 'r') as file:
+            my_query = file.read()
 
-        # Perform a query.
-        QUERY = (
-            'select comment_id, body, pull_request_id from `ghtorrent-bq.ght_2018_04_01.pull_request_comments` where pull_request_id = 19352525')
-        query_job = client.query(QUERY)  # API request
-        rows = query_job.result()  # Waits for query to finish
+        self.logger.info(f"Running BigQuery, query loaded from {self.query_file}: {my_query}")
+        client = bigquery.Client()
+        query_job = client.query(my_query)  # API request
+        rows = query_job.result() # Waits for query to finish
+
+        self.logger.info(f"Running BigQuery, query loaded from {self.query_file}: {my_query}")
+        with open(self.output_csv_file, mode='w', newline='', encoding='utf-8') as output_csvfile:      
+            field_names = [field.name for field in rows.schema]
+            csv_writer = DictWriter(output_csvfile, field_names, delimiter=',')
+            csv_writer.writeheader()                               
 
         for row in rows:
             self.logger.info(f'comment_id: {row.comment_id}, pull_request_id: {row.pull_request_id}, body: {row.body}')
@@ -29,3 +41,4 @@ class CommentsResourceAccess:
         # TODO
         # 1. Detect language, only take English
         # 2. If the comments is longer than x, then try getting from MongoDB.
+        # https://googleapis.github.io/google-cloud-python/latest/bigquery/index.html
