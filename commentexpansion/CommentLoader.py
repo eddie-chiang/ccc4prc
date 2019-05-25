d43e91fcec1d71fe4f35df50fbc7a2f985cda32d
Eddie Chiang <eddiechiang@Surface-Book.local>
2019-05-25 19:27:45 +1200

    Return a single comment.

--- commentexpansion/CommentLoader.py
+++ commentexpansion/CommentLoader.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 from sshtunnel import SSHTunnelForwarder
 
+
 class CommentLoader:
     """A data loader class that expands a truncated pull request comment from GHTorrent MongoDB.
 
@@ -19,40 +20,43 @@ class CommentLoader:
         db (str): MongoDB database
         collection (str): MongoDB collection
     """
-    def __init__(self, 
-            ssh_host: str, 
-            ssh_port: int, 
-            ssh_username: str, 
-            ssh_pkey: str, 
-            ssh_private_key_password: str, 
-            db_host: str,
-            db_port: int,
-            db_username: str,
-            db_password: str,
-            db: str):
+
+    def __init__(self,
+                 ssh_host: str,
+                 ssh_port: int,
+                 ssh_username: str,
+                 ssh_pkey: str,
+                 ssh_private_key_password: str,
+                 db_host: str,
+                 db_port: int,
+                 db_username: str,
+                 db_password: str,
+                 db: str):
         self.logger = logging.getLogger(self.__class__.__name__)
 
         # Creates an SSH tunnel
         self.logger.info(f'Creating an SSH tunnel to {ssh_host}:{ssh_port}')
         self.server = SSHTunnelForwarder((ssh_host, ssh_port),
-                                    ssh_username = ssh_username,
-                                    ssh_pkey = ssh_pkey,
-                                    ssh_private_key_password = db_password,
-                                    remote_bind_address=(db_host, db_port))
+                                         ssh_username=ssh_username,
+                                         ssh_pkey=ssh_pkey,
+                                         ssh_private_key_password=db_password,
+                                         remote_bind_address=(db_host, db_port))
         self.server.start()
-        
+
         client = MongoClient('127.0.0.1',
-                            self.server.local_bind_port,
-                            username = db_username,
-                            password = db_password,
-                            authSource = db,
-                            authMechanism = 'SCRAM-SHA-1')
+                             self.server.local_bind_port,
+                             username=db_username,
+                             password=db_password,
+                             authSource=db,
+                             authMechanism='SCRAM-SHA-1')
         self.db = client[db]
         self.collection = self.db['pull_request_comments']
 
         try:
-            self.logger.info(f'Connecting to MongoDB 127.0.0.1:{self.server.local_bind_port}.')
-            client.admin.command('ismaster') # The ismaster command is cheap and does not require auth.
+            self.logger.info(
+                f'Connecting to MongoDB 127.0.0.1:{self.server.local_bind_port}.')
+            # The ismaster command is cheap and does not require auth.
+            client.admin.command('ismaster')
             self.logger.info('Successfully connected to MongoDB server.')
         except ConnectionFailure:
             self.logger.errors('MongoDB server is not available.')
@@ -61,7 +65,7 @@ class CommentLoader:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.server.stop() # Close SSH tunnel
+        self.server.stop()  # Close SSH tunnel
 
     def Load(self, owner: str, repo: str, pullreq_id: int, comment_id: int):
         """Load the full comment.
@@ -72,14 +76,10 @@ class CommentLoader:
             pullreq_id (int): Pull request ID.
             comment_id (int): Pull request comment ID.
         """
-        query = { 
-            "owner": owner,
-            "repo": repo,
-            "pullreq_id": pullreq_id,
-            "id": comment_id }
-        
-        mydoc = self.collection.find(query)
+        query = {"owner": owner,
+                 "repo": repo,
+                 "pullreq_id": pullreq_id,
+                 "id": comment_id}
 
-        for x in mydoc:
-            self.logger.info(x)
-       
+        doc = self.collection.find_one(query)
+        return doc['body']
