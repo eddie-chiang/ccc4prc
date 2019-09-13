9a461b24789838d9cfb7477f4c815752e1309293
Eddie Chiang <eddiecjc@gmail.com>
2019-09-14 11:30:22 +1200

    Naive Bayes - WIP. Accuracy: 0.7662337662337663.

--- ml/MachineLearning.py
+++ ml/MachineLearning.py
@@ -1,6 +1,7 @@
 import logging
 
 from pandas import DataFrame
+from sklearn import metrics
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
@@ -22,43 +23,44 @@ class MachineLearning:
         Returns:
             model: a trained machine learning model.
         """
-        
+
         # Create a Naive Bayes Gaussian Classifier.
         model = GaussianNB()
-
-        # Split data into training and test sets.
-        y = data_frame['code_comprehension_related']
-        X = data_frame.drop(columns='code_comprehension_related')
-        X_train, X_test, y_train, y_test = train_test_split(X,
-                                                            y,
-                                                            test_size=0.2,  # 20%
-                                                            random_state=2019,  # An arbitrary seed so the results can be reproduced
-                                                            stratify=y)  # Stratify the sample by the target (i.e. code_comprehension_related)
 
         # Declare data preprocessing steps.
         # Encode features, i.e. convert string labels into numbers.
         label_encoder = preprocessing.LabelEncoder()
 
         dialogue_act_classification_ml = data_frame['dialogue_act_classification_ml']
-        dialogue_act_classification_ml_encoded = label_encoder.fit_transform(dialogue_act_classification_ml)
+        dialogue_act_classification_ml_encoded = label_encoder.fit_transform(
+            dialogue_act_classification_ml)
 
         comment_is_by_author = data_frame['comment_is_by_author']
-        comment_is_by_author_encoded = label_encoder.fit_transform(comment_is_by_author)
+        comment_is_by_author_encoded = label_encoder.fit_transform(
+            comment_is_by_author)
 
         code_comprehension_related = data_frame['code_comprehension_related']
         label = label_encoder.fit_transform(code_comprehension_related)
 
         # Combinig features into single listof tuples
-        features = zip(dialogue_act_classification_ml_encoded, comment_is_by_author_encoded)
+        features = list(zip(dialogue_act_classification_ml_encoded,
+                            comment_is_by_author_encoded))
 
-        # Train the model using the training sets
-        model.fit(features, label)
+        # Split data into training and test sets.
+        target = data_frame['code_comprehension_related']
+        X_train, X_test, y_train, y_test = train_test_split(features,
+                                                            target,
+                                                            test_size=0.2,  # 20%
+                                                            random_state=2019,  # An arbitrary seed so the results can be reproduced
+                                                            stratify=target)  # Stratify the sample by the target (i.e. code_comprehension_related)
 
-        #Predict Output
-        predicted = model.predict([[0,2]]) # 0:Overcast, 2:Mild
+        # Train the model using the training sets.
+        model.fit(X_train, y_train)
 
-        # https://www.datacamp.com/community/tutorials/naive-bayes-scikit-learn
+        # Predict the response for test set.
+        y_pred = model.predict(X_test)
 
-        # pipeline = make_pipeline(preprocessing.StandardScaler(), )
+        # Model accuracy, how often is the classifier correct?
+        self.logger.info(f'Accuracy: {metrics.accuracy_score(y_test, y_pred)}')
 
         return model
